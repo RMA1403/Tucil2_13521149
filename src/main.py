6ffa14142d3e6ae01ed320ac35ad42e2823b60bb
Rava Maulana <ravamaulana14@gmail.com>
2023-02-27 23:11:09 +0700

    Integrated with main program

--- src/main.py
+++ src/main.py
@@ -2,12 +2,22 @@ from functions.closest_pair import closest_pair
 from functions.sorting import quicksort_on_x
 from functions.utility import getRandomPoints
 from functions.visualizer import visualize
+from functions.input_output import get_user_input, print_output
+from functions.brute_force import brute_force
 
 def main() -> None:
   """Main function for this program"""
-  p = getRandomPoints(16, 3)
-  d, p1, p2 = closest_pair(quicksort_on_x(p))
-  visualize(p, p1, p2)
+  num_points, dimension = get_user_input()
+  points = getRandomPoints(num_points, dimension)
+
+  sorted_points = quicksort_on_x(points)
+  min_distance_dnc, point_1_dnc, point_2_dnc = closest_pair(sorted_points)
+
+  min_distance_bf, point_1_bf, point_2_bf = brute_force(points)
+
+  print_output(min_distance_dnc, point_1_dnc, point_2_dnc, 1, min_distance_bf, point_1_bf, point_2_bf, 1, 1.0)
+
+  # visualize(p, p1, p2)
 
 if __name__ == "__main__":
   main()
